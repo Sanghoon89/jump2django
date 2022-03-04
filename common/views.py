9db4508e55ecd218423508de6dc90141f08dc37f
Hooni <msh2355@naver.com>
2022-03-04 22:59:40 +0900

    common views

--- common/views.py
+++ common/views.py
@@ -5,14 +5,18 @@ from common.forms import UserForm
 # Create your views here.
 
 def signup(request):
-    form = UserForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        raw_password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=raw_password)
-        login(request, user)
-        return redirect('index')
+    '''
+    회원가입
+    '''
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form':form})
