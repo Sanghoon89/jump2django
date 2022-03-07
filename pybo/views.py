b98b56938b5b87f4ddec9c6b4cb97c669ed60d3d
Hooni <msh2355@naver.com>
2022-03-07 21:23:06 +0900

    pybo views

--- pybo/views.py
+++ pybo/views.py
@@ -6,7 +6,7 @@ from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_reqiured
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """
